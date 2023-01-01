86b9705a975a5e1d8cbcf9634b4db3be6640bbf1
wajahat-dev <aliwajahat021@gmail.com>
2023-01-01 20:46:03 +0500

    code updated

--- src/config.js
+++
@@ -1,8 +0,0 @@
-// export const imageUrl = process.env.NODE_ENV ===  'development' ? 'http://localhost:8000' : process.env.REACT_APP_IMAGE_URL;
-// export const baseUrl = process.env.NODE_ENV ===  'development' ?  `${imageUrl}/api`: `/api`;
-// baseUrl = heroku.com/api
-
-// export const baseUrl = process.env.REACT_APP_BASEURL || 'http://localhost:8000/api';
-
-export const imageUrl = process.env.REACT_APP_IMAGE_URL || '' ;
-export const baseUrl = process.env.REACT_APP_BASEURL || `${imageUrl}/api`;
--- src/store/actions/ui.js
+++
@@ -1,5 +0,0 @@
-export const HIDE_FORM = "HIDE_FORM";
-export const SHOW_FORM = "SHOW_FORM";
-
-export const showForm = () => ({ type: SHOW_FORM });
-export const hideForm = () => ({ type: HIDE_FORM });
